ede7a55f0f269ea4ba9eec1e5e6f625db65c23f9
Devon <k849996781@vip.qq.com>
2018-01-20 10:15:30 +0800

    no message

--- spider/spiders/_user_spider.py
+++ spider/spiders/_user_spider.py
@@ -6,21 +6,46 @@ class UserSpider(scrapy.Spider):
 
     def start_requests(self):
         urls = [
-            'https://www.proginn.com/wo/122308'
+            'https://www.proginn.com/1/',
+            'https://www.proginn.com/2/',
+            'https://www.proginn.com/3/',
+            'https://www.proginn.com/4/',
+            'https://www.proginn.com/5/',
+            'https://www.proginn.com/6/',
+            'https://www.proginn.com/7/',
+            'https://www.proginn.com/8/'
         ]
-        for url in urls:
-            yield scrapy.Request(url=url, callback=self.parse)
+
+        maxLen = 0;
+
+        while maxLen < 500:
+            maxLen+=1
+            url = 'https://www.proginn.com/' + str( maxLen )
+            yield scrapy.Request(url=url, callback=self.parse_list)
+
+        
+    def parse_list(self, response):
+        page = response.url.split("/")[-2]
+        filename = 'user-list.html'
+
+        userlist = response.css('.user-info .title a::attr(href)')
+
+        for userUrl in userlist:
+            if userUrl.extract() != '/hire/fast':
+                with open(filename, 'ab+') as f:
+                    f.write(userUrl.extract().encode("UTF-8")+'\n\n')
+                    yield scrapy.Request(url=userUrl.extract(), callback=self.parse)
+
+        
 
     def parse(self, response):
         page = response.url.split("/")[-2]
-        filename = 'user-%s.html' % page
-        with open(filename, 'wb') as f:
+        filename = 'user-data.html'
+        with open(filename, 'ab+') as f:
             
             coll = _db_.get_db().people
-            #coll.insert({'name': response.css('.avatar .image img::attr(alt)').extract_first(),'url':response.css('.avatar .image img::attr(src)').extract_first()})
-
             #avatar url
-            f.write(response.css('.avatar .image img::attr(src)').extract_first().encode("UTF-8")+'\n\n')
+            f.write('<hr/>'+response.css('.avatar .image img::attr(src)').extract_first().encode("UTF-8")+'\n\n')
 
             #user name
             f.write(response.css('.avatar .image img::attr(alt)').extract_first().encode("UTF-8")+'\n\n')
@@ -35,40 +60,48 @@ class UserSpider(scrapy.Spider):
             f.write(response.css('.hire-info p::text')[2].extract().encode("UTF-8")+'\n\n')
             
             #user dec
-            f.write(response.css('.panel>.content p::text').extract_first().encode("UTF-8")+'\n\n')
+            dec = response.css('.panel>.content p::text')
+            if dec:
+                f.write(dec.extract_first().encode("UTF-8")+'\n\n')
+            
+            
             
             #user worklist
-            worklist = response.css('.J_Works')[0].css('li')
+            if len( response.css('.J_Works') ) >= 1:
+                worklist = response.css('.J_Works')[0].css('li')
 
-            for work in worklist:
-                f.write(work.css('.title span::text')[0].extract().encode("UTF-8")+'\n\n')
-                f.write(work.css('.title span::text')[1].extract().encode("UTF-8")+'\n\n')
-                f.write(work.css('.title span::text')[2].extract().encode("UTF-8")+'\n\n')
-                f.write(work.css('.summary::text')[0].extract().encode("UTF-8")+'\n\n')
-            
+                for work in worklist:
+                    f.write(work.css('.title span::text').extract_first().encode("UTF-8")+'\n\n')
+                    f.write(work.css('.title span::text')[1].extract().encode("UTF-8")+'\n\n')
+                    f.write(work.css('.title span::text')[2].extract().encode("UTF-8")+'\n\n')
+                    f.write(work.css('.summary::text').extract_first().encode("UTF-8")+'\n\n')
+                
             #user edulist
-            edulist = response.css('.J_Works')[1].css('li')
+            if len( response.css('.J_Works') ) >= 2:
+                edulist = response.css('.J_Works')[1].css('li')
 
-            for edu in edulist:
-                f.write(edu.css('.title span::text')[0].extract().encode("UTF-8")+'\n\n')
-                f.write(edu.css('.title span::text')[1].extract().encode("UTF-8")+'\n\n')
-                f.write(edu.css('.title span::text')[2].extract().encode("UTF-8")+'\n\n')
-                f.write(edu.css('.summary::text')[0].extract().encode("UTF-8")+'\n\n')
- 
+                for edu in edulist:
+                    f.write(edu.css('.title span::text').extract_first().encode("UTF-8")+'\n\n')
+                    f.write(edu.css('.title span::text')[1].extract().encode("UTF-8")+'\n\n')
+                    f.write(edu.css('.title span::text')[2].extract().encode("UTF-8")+'\n\n')
+                    f.write(edu.css('.summary::text').extract_first().encode("UTF-8")+'\n\n')
+     
             #user skilllist
             skilllist = response.css('.skill-list .skill')
 
             for skill in skilllist:
-                f.write(skill.css('.name::text')[0].extract().strip().encode("UTF-8")+'\n\n')
-                f.write(skill.css('.progress div::attr(class)')[0].extract().encode("UTF-8")+'\n\n')
+                f.write(skill.css('.name::text').extract_first().strip().encode("UTF-8")+'\n\n')
+                f.write(skill.css('.progress div::attr(class)').extract_first().encode("UTF-8")+'\n\n')
 
             #user works
-            userwork = response.css('.works .work')
+            if response.css('.works .work'):
+                userwork = response.css('.works .work')
 
-            for work in userwork:
-                f.write(work.css('.title a::text')[0].extract().encode("UTF-8")+'\n\n')
-                f.write(work.css('.duty::text')[0].extract().encode("UTF-8")+'\n\n')
-                f.write(work.css('.description::text')[0].extract().strip().encode("UTF-8")+'\n\n')
-                print work.css('.J_ImgPop::attr(href)').extract()[0]
-                for workimg in work.css('.J_ImgPop::attr(href)'):
-                    f.write(workimg.extract().strip().encode("UTF-8")+'\n\n')
+                for work in userwork:
+                    f.write(work.css('.title a::text').extract_first().encode("UTF-8")+'\n\n')
+                    f.write(work.css('.description::text').extract_first().strip().encode("UTF-8")+'\n\n')
+
+                    coll.insert({'url' : work.css('.J_ImgPop::attr(href)').extract()})
+
+                    for workimg in work.css('.J_ImgPop::attr(href)'):
+                        f.write(workimg.extract().strip().encode("UTF-8")+'\n\n')
