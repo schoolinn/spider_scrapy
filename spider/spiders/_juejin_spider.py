f4183f7ed9967ce0bdddf1827f7ccca3b2135d54
Devon <k849996781@vip.qq.com>
2018-01-22 00:03:07 +0800

    no message

--- spider/spiders/_juejin_spider.py
+++ spider/spiders/_juejin_spider.py
@@ -1,4 +1,5 @@
 import scrapy
+from scrapy.selector import Selector
 import json
 from ..items import juejinItem
 
@@ -10,16 +11,13 @@ class JuejinSpider(scrapy.Spider):
         self.item = juejinItem()
 
     def start_requests(self):
-        urls = [
-        'https://juejin.im/subscribe/all'
-        ]
-        for url in urls:
-            yield scrapy.Request(url=url, callback=self.parse)
-
-    def parse(self, response):
-        for quote in response.css('.item .tag').re(r'st:state[=\'\"\s]+([^\'\"]*)[\'\"]?[\s\S]*'):
+        
+        fo = open("juejin.html", "r+")
+        str = fo.read()
+        for quote in Selector(text=str).css('.item .tag').re(r'st:state[=\'\"\s]+([^\'\"]*)[\'\"]?[\s\S]*'):
             tag_url = "https://timeline-merger-ms.juejin.im/v1/get_tag_entry?src=web&tagId="+ quote +"&page=1&pageSize=10&sort=rankIndex"
             yield scrapy.Request(url=tag_url, callback=self.parse_c)
+
 
 
     def parse_c(self, response):
